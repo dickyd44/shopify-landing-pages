93baa7f002a5dcccd9d04055548a38822035f400
dikydrmwn08@gmail.com <dikydrmwn08@gmail.com>
2024-03-17 22:06:54 +0800

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer";
+import Hero from "./components/Main";
 import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Hero />
       <Footer />
     </>
   );

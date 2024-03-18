2684399dcf6968dd5f0a8638cb6be744528a1013
dikydrmwn08@gmail.com <dikydrmwn08@gmail.com>
2024-03-18 21:50:20 +0800

    setup banner section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import Footer from "./components/Footer";
-import Hero from "./components/Main";
 import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import Banner from "./components/Banner";
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <Hero />
+      <Main />
+      <Banner />
       <Footer />
     </>
   );
